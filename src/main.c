e87eba79cf7eaaeed0885276f80a34843958b8ff
igorp-lopes <igorp.lopes16@gmail.com>
2020-03-16 21:00:59 -0300

    Commit inicial + definição de variáveis e funções

--- src/main.c
+++ src/main.c
@@ -1,14 +1,25 @@
 /* Data de submissao:
- * Nome:
- * RA:
+ * Nome: Igor Peterossi Lopes
+ * RA: 174929
  */
 
 #include <stdio.h>
-const int tam_buffer=100;
+
+int string_length(char*);
+void reverse_words(char*);
+
+const int tam_buffer = 100; /* Tamanho máximo da string que pode ser armazenada */
 
 int main() {
-  char buffer[tam_buffer];
-  fgets(buffer, tam_buffer, stdin);
+
+  char buffer[tam_buffer]; /* Buffer que armazena a string que será trabalhada */
+  char temp = 0; /* Variável auxiliar que armazena caracteres da string */
+  int i, j, k; /* Variáveis que armazenam as posições de começo e fim da palavra */
+  int len = 0; /* Variável que armazena o tamanho da string */
+
+  fgets(buffer, tam_buffer, stdin); /* Recebemos pelo console a string */
   printf("%s", buffer);
+
   return 0;
+
 }
